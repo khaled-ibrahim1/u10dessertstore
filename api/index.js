33c1a7d5f435aed4f9cf7bc961203183aeff1eb3
khaled-ibrahim1 <72127751+khaled-ibrahim1@users.noreply.github.com>
2022-11-02 23:24:51 +0100

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,1 +1,38 @@
+const express = require("express");
+const dotenv = require("dotenv");
 
+const app = express();
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+const path = require("path")
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DBconnected!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
+  res.json("server start")
+})
+
+app.listen(PORT, console.log(`Server is starting at ${PORT}`));
